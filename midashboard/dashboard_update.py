5e74ba13bc9fb5fcf5b403578064992d1cba770f
Dennis Ying <dennis.ying@ni.com>
2019-10-15 23:07:47 +0800

    Update dashboard in a separate thread.

--- midashboard/dashboard_update.py
+++ midashboard/dashboard_update.py
@@ -1,32 +1,51 @@
 from datetime import datetime
 from dashboard import Dashboard
 from urllib3 import disable_warnings, exceptions
+import threading
+import time
 
 
 def log_with_timestamp(msg):
     print(f'[{datetime.now()}] {msg}')
 
 
-def dashboard_update():
-    log_with_timestamp("Start to update dashboard.")
-    dashboard = Dashboard()
-    succeeded = False
-    max_retry = 5
-    for i in range(0, max_retry):
-        try:
-            dashboard.update()
-        except Exception as e:
-            log_with_timestamp(f'Failed to update dashboard. [{e}]')
-            continue
-        succeeded = True
-        break
-    if succeeded:
-        log_with_timestamp('Dashboard data updated.')
-    else:
-        log_with_timestamp(f'Dashboard data failed to update after {max_retry} retries.')
+class DashboardUpdater(threading.Thread):
+    def __init__(self, interval_min, max_updates=-1):
+        threading.Thread.__init__(self)
+        self.interval_min = interval_min
+        self.max_updates = max_updates
+
+    def run(self):
+        count = 0
+        while True:
+            self._dashboard_update()
+            if self.max_updates > 0:
+                count += 1
+                if count >= self.max_updates:
+                    break
+            time.sleep(self.interval_min * 60)
+
+    @staticmethod
+    def _dashboard_update():
+        log_with_timestamp("Start to update dashboard.")
+        dashboard = Dashboard()
+        succeeded = False
+        max_retry = 5
+        for i in range(0, max_retry):
+            try:
+                dashboard.update()
+            except Exception as e:
+                log_with_timestamp(f'Failed to update dashboard. [{e}]')
+                continue
+            succeeded = True
+            break
+        if succeeded:
+            log_with_timestamp('Dashboard data updated.')
+        else:
+            log_with_timestamp(f'Dashboard data failed to update after {max_retry} retries.')
 
 
 if __name__ == '__main__':
     disable_warnings(exceptions.InsecureRequestWarning)
-    dashboard_update()
+    DashboardUpdater(interval_min=1, max_updates=2).start()
 
