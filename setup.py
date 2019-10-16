dec13fa2003958172251c39182d7612d97d0229f
Dennis Ying <dennis.ying@ni.com>
2019-10-17 00:18:12 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="midashboard-ysomebody",
-    version="1.0.0dev5",
+    version="1.0.0dev6",
     author="ysomebody",
     author_email="ysomebody@163.com",
     description="MI dashboard",
