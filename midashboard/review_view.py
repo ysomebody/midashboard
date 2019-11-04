7aa682e8c0850e2525fa38e78d40239e23ec08fb
Dennis Ying <dennis.ying@ni.com>
2019-11-04 10:57:00 +0800

    Update review view according to the statistic change. Add hover info.

--- midashboard/review_view.py
+++ midashboard/review_view.py
@@ -3,31 +3,66 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
 import json
+from enum import Enum
+import textwrap
+from collections import defaultdict
 
 
 class ReviewView(object):
+    ColorPalette = Enum('ColorPalette', 'category people')
+
     def __init__(self, data):
         self.data = data
-        self.default_layout = dict(height=200, margin=dict(l=10, t=20, b=0, r=10))
+        self.default_layout = dict(height=250, margin=dict(l=10, t=20, b=0, r=10))
         # colors matching: unresolved, internal, owner, submitted
-        self.counter_colors = ['OrangeRed', 'DarkOrange', 'SkyBlue', 'Gray']
+        self.category_colors = {
+            'Unresolved': 'OrangeRed',
+            'Internal': 'DarkOrange',
+            'Code Owner': 'SkyBlue',
+            'Submitted': 'Gray'
+        }
         self.people_colors = ['#FFCD24', '#2AABE4', '#9DC8E4', '#666B6E', '#FFC2BB', '#FF8883', '#BC4123', '#0B172A', '#463940', '#03393D']
 
     @staticmethod
     def _get_title(title_text):
         return dict(text=title_text, position='top center', font=dict(size=20))
 
-    def get_overall_figure(self):
-        overall_data = self.data['status']
-        count = sum([int(s['count']) for s in overall_data])
+    def _map_colors(self, labels):
+        return [self.category_colors[l] for l in labels]
+
+    @staticmethod
+    def _brief_detail_string(detail):
+        url = detail['url'].rsplit('/', 2)[1]
+        summary = detail['summary']
+        brief = f'[{url}] {summary}'
+        return textwrap.shorten(brief, width=100)
+
+    def _get_hover_text(self, details):
+        hover_text = []
+        for details_of_the_label in details:
+            text_of_the_label = '<br>'.join([self._brief_detail_string(d) for d in details_of_the_label])
+            hover_text.append(text_of_the_label)
+        return hover_text
+
+    def get_pie(self, key, color=ColorPalette.people):
+        data = self.data[key]
+        total_count = sum([v['count'] for v in data.values()])
+        title = f'{key} ({total_count})'
+        labels = list(data.keys())
+        if color == self.ColorPalette.people:
+            colors = self.people_colors
+        elif color == self.ColorPalette.category:
+            colors = self._map_colors(labels)
+        hover_text = self._get_hover_text([v['details'] for v in data.values()])
         return go.Figure(
             data=go.Pie(
-                title=self._get_title(f'{count} open reviews'),
-                labels=[s['description'] for s in overall_data],
-                values=[s['count'] for s in overall_data],
-                marker_colors=self.counter_colors,
-                hoverinfo='label',
+                title=self._get_title(title),
+                labels=labels,
+                values=[v['count'] for v in data.values()],
+                marker_colors=colors,
                 textinfo='value',
+                hoverinfo='text',
+                hovertext=hover_text,
                 showlegend=True,
                 textfont_size=15,
                 pull=0.02,
@@ -37,57 +72,37 @@ class ReviewView(object):
             layout={**self.default_layout, **dict(legend=dict(x=0.85))}
         )
 
-    def get_developers_figure(self):
-        return go.Figure(
-            data=go.Pie(
-                title=self._get_title('By developers'),
-                labels=list(self.data["developers"].keys()),
-                values=list(self.data["developers"].values()),
-                hoverinfo='label+value',
-                textinfo='value',
-                showlegend=True,
-                textfont_size=15,
-                pull=0.03,
-                marker_colors=self.people_colors,
-            ),
-            layout=self.default_layout
-        )
-
-    def get_reviewers_figure(self):
-        return go.Figure(
-            data=go.Pie(
-                title=self._get_title('By reviewers'),
-                labels=list(self.data["reviewers"].keys()),
-                values=list(self.data["reviewers"].values()),
-                hoverinfo='label+value',
-                textinfo='value',
-                showlegend=True,
-                textfont_size=15,
-                pull=0.03,
-                marker_colors=self.people_colors,
-            ),
-            layout=self.default_layout
-        )
-
-    def get_duration_figure(self):
-        duration = self.data['duration']
-        days = duration['names']
-        data = duration['data']
+    def get_stacked_bar(self, key):
+        data = self.data[key]
+        x_max = max(list(map(int, data.keys())))
+        x_names = [f'{x} day' + ('s' if x > 1 else '') for x in range(1, x_max + 1)]
+        bars = defaultdict(lambda: [0] * x_max)
+        hover_text = defaultdict(lambda: [''] * x_max)
+        for day, categories in data.items():
+            for category, v in categories.items():
+                bars[category][int(day) - 1] = int(v['count'])
+                hover_text[category][int(day) - 1] = '<br>'.join([self._brief_detail_string(d) for d in v['details']])
+        colors = self._map_colors(bars.keys())
         duration_fig = go.Figure(
             data=[
                 go.Bar(
-                    name=d['status'],
-                    x=days,
-                    y=d['values'],
-                    marker_color=c
+                    name=k,
+                    x=x_names,
+                    y=v,
+                    hoverinfo='text',
+                    hovertext=hover_text[k],
+                    marker_color=self.category_colors[k]
                 )
-                for c, d in zip(self.counter_colors, data)
+                for k, v in bars.items()
             ],
             layout=self.default_layout
         )
 
         # Change the bar mode
-        duration_fig.update_layout(barmode='stack')
+        duration_fig.update_layout(
+            title= key,
+            barmode='stack'
+        )
         return duration_fig
 
     def refresh(self, new_data):
@@ -98,19 +113,19 @@ class ReviewView(object):
             children=[
                 html.Div(
                     children=[
-                        dcc.Graph(figure=self.get_overall_figure())
+                        dcc.Graph(figure=self.get_pie('Overall', ReviewView.ColorPalette.category))
                     ]
                 ),
                 html.Div(
                     children=[
-                        dcc.Graph(figure=self.get_developers_figure()),
-                        dcc.Graph(figure=self.get_reviewers_figure()),
+                        dcc.Graph(figure=self.get_pie('Unresolved Reviews')),
+                        dcc.Graph(figure=self.get_pie('Inactive Reviews')),
                     ],
                     style={'columnCount': 2, 'rowCount': 1}
                 ),
                 html.Div(
                     children=[
-                        dcc.Graph(figure=self.get_duration_figure())
+                        dcc.Graph(figure=self.get_stacked_bar('Days Since Posted'))
                     ]
                 )
             ],
@@ -122,4 +137,4 @@ if __name__ == '__main__':
     with open(r'..\data\dashboard_data.json') as f:
         data = json.load(f)
     r = ReviewView(data['review'])
-    r.get_developers_figure().write_html('test.html', auto_open=True)
+    r.get_stacked_bar('Days Since Posted').write_html('test.html', auto_open=True)
